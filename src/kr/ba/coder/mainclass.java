a82e7183170f94c2851af04a03782ba453c03413
leehg <leehg@DESKTOP-SU34KMU>
2018-10-18 17:56:44 +0900

    init

---
+++ src/kr/ba/coder/mainclass.java
@@ -0,0 +1,13 @@
+package kr.ba.coder;
+
+public class mainclass {
+	public static void main(String[] args) {
+		int aval = 10;
+		int bval = 5;
+		int Tot;
+		
+		Tot=aval+bval;
+		System.out.println(Tot);
+		System.out.println(Tot);
+	}
+}
